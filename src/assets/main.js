f85d606b546849a9abf9850ecff268db604f4a31
stas7t <stas7t@gmail.com>
2017-05-06 13:42:36 +0300

    Create validateInput function

--- src/assets/main.js
+++ src/assets/main.js
@@ -21,3 +21,12 @@ function setHiddenFields() {
 function setMessage(message) {
     document.getElementById('message').innerHTML = message
 }
+
+function validateInput(input) {
+    if (input.length === 4) {
+        return true
+    } else {
+      setMessage("Guesses must be exactly 4 characters long.")
+      return false
+    }
+}
