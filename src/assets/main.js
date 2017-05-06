9c2aa944101d6e0cc449b6ea4f5a5dffa877d989
stas7t <stas7t@gmail.com>
2017-05-06 13:33:11 +0300

    move setHiddenFields call to guess func

--- src/assets/main.js
+++ src/assets/main.js
@@ -4,17 +4,16 @@ let attempt = document.getElementById('attempt');
 function guess() {
     let input = document.getElementById('user-guess');
     //add functionality to guess function here
+    if (answer.value === "" || attempt.value === "") {
+      setHiddenFields()
+    }
 }
 
 //implement new functions here
 function setHiddenFields() {
-  if (answer.value === "" || attempt.value === "") {
-      answer.value = Math.floor(Math.random()*10000);
-      while (answer.value.length < 4) {
-        answer.value = "0" + answer.value;
-      }
-      attempt.value = 0;
-  }
+    answer.value = Math.floor(Math.random()*10000);
+    while (answer.value.length < 4) {
+      answer.value = "0" + answer.value;
+    }
+    attempt.value = 0;
 }
-
-setHiddenFields()
