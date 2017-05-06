21cb431bed0bba00547ec472baa480e03e3924f2
stas7t <stas7t@gmail.com>
2017-05-06 12:49:57 +0300

    Fix setHiddenFields Function

--- src/assets/main.js
+++ src/assets/main.js
@@ -8,11 +8,13 @@ function guess() {
 
 //implement new functions here
 function setHiddenFields() {
-  if (answer === "" || attempt === "") {
-      answer = Math.floor(Math.random()*10000);
-      while (answer.length < 4) {
-        answer = "0" + answer;
+  if (answer.value === "" || attempt.value === "") {
+      answer.value = Math.floor(Math.random()*10000);
+      while (answer.value.length < 4) {
+        answer.value = "0" + answer.value;
       }
-      attempt = 0;
+      attempt.value = 0;
   }
 }
+
+setHiddenFields()
